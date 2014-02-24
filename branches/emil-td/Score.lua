b64f1ab780418588a525bb5bad4c14b622b59168
emil.rauff.andersen@gmail.com <emil.rauff.andersen@gmail.com@50f398b4-9be7-c980-fbfd-07c193cab47a>
2014-02-24 18:22:01 +0000

    done mit deinem menu!

--- branches/emil-td/Score.lua
+++ branches/emil-td/Score.lua
@@ -16,11 +16,13 @@ function Score_load()
 	scores.initialise(10, true)
 	Score_uBackButton = love.graphics.newImage('Sprites/Buttons/scoreMenu/uBack.png')
 	Score_hBackButton = love.graphics.newImage('Sprites/Buttons/scoreMenu/hBack.png')
+	score_font = love.graphics.newFont('Fonts/towerruinscond.ttf', 35)
 end
 
 function Score_draw()
 	love.graphics.draw(mainMenuBG, 0, 0)
 	HoverMenu(0, 562, 150, 38, Score_uBackButton, Score_hBackButton)
+	love.graphics.setFont(score_font)
 	score_printScreen(50, 320, 590, -20, 50)
 end
 
