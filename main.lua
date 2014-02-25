13af4bd42bf73c4b336574ab537697b55a43551e
joachimandersen11@gmail.com <joachimandersen11@gmail.com@50f398b4-9be7-c980-fbfd-07c193cab47a>
2014-02-25 20:01:59 +0000

    Initial scheme main file with basic LÖVE callback handlers

--- main.lua
+++ main.lua
@@ -1,1 +1,82 @@
-hej
+--[[
+
+	main.lua
+		
+	* DEVELOPERS: 	Joachim Andersen
+					Emil Plovmand
+
+--]]
+
+--[[
+
+	Main file, contains all game logic and callbacks
+	
+--]]
+
+--[[
+
+	Screen-render handlers
+	
+--]]
+
+local t_RenderItems = {}
+render = {}
+
+function love.draw ()
+	for strItemID, t_Item in ipairs (t_RenderItems) do
+		love.graphics[t_Item.method](unpack(t_Item))
+	end
+	
+	return true
+end
+
+function render.add (strID, strMethod, ...)
+	t_RenderItems[strID] = {['method'] = strMethod, ...}
+	return true
+end
+
+function render.modify (strID, ...)
+	t_RenderItems[strID] = {['method'] = t_RenderItems[strID], ...}
+	return true
+end
+
+function render.remove (strID)
+	t_RenderItems[strID] = nil
+	return true
+end
+
+--[[
+
+	Game process handlers
+	
+--]]
+
+local t_ProcessCallbacks = {}
+
+function love.update (dt)
+	for i,strCallbackFunc in ipairs (t_ProcessCallbacks) do
+		_G[strCallbackFunc](dt)
+	end
+	
+	return true
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
